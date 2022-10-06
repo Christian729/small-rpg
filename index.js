4733a80fd1e4079af03850b0f3a93df6d86a4f2a
Christian Lozano <christianlozano729@gmail.com>
2022-10-06 18:53:31 -0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,12 @@
+const canvas = document.querySelector('canvas');
+
+const c = canvas.getContext('2d')// this allows us to access a 2d animation API so we can
+// do some cool 2d stuff
+
+canvas.width = 1024 // this allows us to have a screen size that should 
+//fit most screens with a good aspect ratio
+canvas.height = 576
+
+c.fillStyle= 'white'
+c.fillRect(0, 0, canvas.width, canvas.height)
+
